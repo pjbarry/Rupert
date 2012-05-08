7e02409addf3dcbf8d3f8dcd388a2889125d1245
PJ <pjbarry@gmail.com>
2012-05-08 16:54:31 +0100

    Cleaned out default cruft, added basic SpecFlow feature

---
+++ RupertUIFeatures/Network.feature
@@ -0,0 +1,12 @@
+﻿Feature: Networking
+	In order to set up and configure Rupert
+	As an administrator
+	I want to see which machines are available to use Rupert
+
+
+Scenario: View possible Murdochs
+	Given I have entered opened the configuration screen
+	When I click 'View Network'
+	Then a list of machines on the network should appear 
+	And an indicator wether each machine is a Murdoch should be shown
+
---
+++ RupertUIFeatures/step_definitions/Network.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace RupertUIFeatures.step_definitions
+{
+    [Binding]
+    public class Network
+    {
+        public class StepDefinitions
+        {
+            [Given(@"I have entered opened the configuration screen")]
+            public void GivenIHaveEnteredOpenedTheConfigurationScreen()
+            {
+                ScenarioContext.Current.Pending();
+            }
+     
+            [When(@"I click 'View Network'")]
+            public void WhenIClickViewNetwork()
+            {
+                ScenarioContext.Current.Pending();
+            }
+       
+            [Then(@"a list of machines on the network should appear")]
+            public void ThenAListOfMachinesOnTheNetworkShouldAppear()
+            {
+                ScenarioContext.Current.Pending();
+            }
+     
+            [Then(@"an indicator wether each machine is a Murdoch should be shown")]
+            public void ThenAnIndicatorWetherEachMachineIsAMurdochShouldBeShown()
+            {
+                ScenarioContext.Current.Pending();
+            }
+        }
+
+    }
+}
