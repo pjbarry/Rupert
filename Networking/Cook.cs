cd0355cbebfbca40e369037031d34b752ed10ff9
PJ <pjbarry@gmail.com>
2012-05-09 17:05:39 +0100

    added projects for spikes, networking and tests

---
+++ Networking/Cook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+
+//Cook is responsible for Network Discovery and Enumeration
+
+namespace Networking
+{
+    internal class Cook
+    {
+        public void Enumerate()
+        {
+            ArrayList alNetworkComputers =
+                NetApi32.GetServerList(NetApi32.SV_101_TYPES.SV_TYPE_WORKSTATION | NetApi32.SV_101_TYPES.SV_TYPE_SERVER);
+
+            for (int nIndex = 0; nIndex < alNetworkComputers.Count; nIndex++)
+            {
+                string computerName = ((NetApi32.SERVER_INFO_101) alNetworkComputers[nIndex]).sv101_name;
+                Console.WriteLine(Dns.GetHostAddresses(computerName)[0] + "\t\t : \t" + computerName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Spikes/Networkspikes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.DirectoryServices
+
+namespace Spikes
+{
+    class Networkspikes
+    {
+        static List<string> GetComputersOnNetwork()
+        {
+            List<string> list = new List<string>();
+            using (DirectoryEntry root = new DirectoryEntry("WinNT:"))
+            {
+                foreach (DirectoryEntry computers in root.Children)
+                {
+                    foreach (DirectoryEntry computer in computers.Children)
+                    {
+                        if ((computer.Name != "Schema"))
+                        {
+                            list.Add(computer.Name);
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
